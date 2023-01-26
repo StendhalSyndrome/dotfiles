71506b57c93f96ceac121a241109f13bd6d21e54
Syndrome <stendhalxsyndrome@protonmail.com>
2023-01-26 20:50:57 +0100

    updated the install script

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pacman -Sy git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl
-mv 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+pacman -Sy nitrogen git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl xf86-input-libinput libinput
+cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 sudo systemctl start --now bluetooth.service
-cd ~/dotfiles
+cd ..
 stow *
