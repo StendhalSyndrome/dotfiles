0f39d7965cc3c5ea78db7a5ee590e505967dda83
Syndrome <stendhalxsyndrome@protonmail.com>
2023-01-28 22:09:50 +0100

    added lightdm theme

--- install/install/install.sh
+++ install/install/install.sh
@@ -2,6 +2,9 @@
 
 sudo pacman -Sy python-pip ttf-ubuntu-mono-nerd wget stow nitrogen git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl xf86-input-libinput libinput
 sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+sudo cp lightdm.conf /etc/lightdm/
+sudo cp lightdm-webkit2-greeter.conf /etc/lightdm/
+sudo cp ./../wallpapers/wallpapers/wallpaper.png /usr/share/backgrounds
 sudo systemctl start --now bluetooth.service 
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 pip install iwlib
