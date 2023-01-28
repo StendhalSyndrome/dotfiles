451e5f03cd277143fcce136d3fd29127ac229e1b
Syndrome <stendhalxsyndrome@protonmail.com>
2023-01-28 22:14:57 +0100

    installs picom

--- install/install/install.sh
+++ install/install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Sy lightdm-webkit-theme-litarvan lightdm-webkit2-greeter python-pip ttf-ubuntu-mono-nerd wget stow nitrogen git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl xf86-input-libinput libinput
+sudo pacman -Sy picom lightdm-webkit-theme-litarvan lightdm-webkit2-greeter python-pip ttf-ubuntu-mono-nerd wget stow nitrogen git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl xf86-input-libinput libinput
 sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 sudo cp lightdm.conf /etc/lightdm/
 sudo cp lightdm-webkit2-greeter.conf /etc/lightdm/
