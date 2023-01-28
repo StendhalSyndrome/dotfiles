bc895f6b39d1b23d4069e06f1c0832ad26ff7896
Syndrome <stendhalxsyndrome@protonmail.com>
2023-01-28 19:09:37 +0100

    mv install files into a folder

---
+++ install/install/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo pacman -Sy python-pip ttf-ubuntu-mono-nerd wget stow nitrogen git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl xf86-input-libinput libinput
+sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+sudo systemctl start --now bluetooth.service 
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+pip install iwlib
+cd ../..
+stow --adopt *
+git restore .
+stow *
