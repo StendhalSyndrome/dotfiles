e058b3587ffefba6a57d7c180b0cb89ed67c10bf
Syndrome <stendhalxsyndrome@protonmail.com>
2023-01-28 17:25:38 +0100

    added oh-my-zsh config and stow --adopt to avoid conflict with existing files

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-pacman -Sy nitrogen git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl xf86-input-libinput libinput
-cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+sudo pacman -Sy wget stow nitrogen git firefox unzip zsh kitty bpytop bluez bluez-utils pamixer noto-fonts-emoji rofi iwd ranger qtile xdotool xclip rofi-emoji brightnessctl xf86-input-libinput libinput
+sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 sudo systemctl start --now bluetooth.service
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 cd ..
+stow --adopt *
+git restore .
 stow *
