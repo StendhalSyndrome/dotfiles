b4a0f1053b0ae3460f6395ee150b5567543353a8
Syndrome <stendhalxsyndrome@protonmail.com>
2023-04-26 15:00:16 +0200

    major update of the install script

--- install/install/install.sh
+++ install/install/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-installdir=echo pwd
 
 welcomemsg() {
     echo "Welcome to Omega Installation script. This script is intended to run on a archinstall-ed machine with qtile, pipewire, NetworkManager and Xorg already setup."
+}
+
+endmsg() {
+    echo "Omega was successfuly installed !"
 }
 
 getuser() {
@@ -22,43 +25,52 @@ getuser() {
     done
 }
 
-getdotfilesdir() {
-    dotfilesdir="/home/$name/dotfiles"
-}
-
 pacmanconf() {
+    # Enables parallel downloads and pacman animation in the loading bar
+    echo "Configuring pacman..."
     sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
     sudo sed -i "/^CheckSpace$/a ILoveCandy" /etc/pacman.conf 
+    echo "Done."
 }
 
 laptopconf() {
+    # Configure the touchpad so double click and other functionalities work
     sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 }
 
 lightdmconf() {
+    # Sets lightdm theme
+    echo "Configuring lightdm..."
     sudo cp lightdm.conf /etc/lightdm/
     sudo cp lightdm-webkit2-greeter.conf /etc/lightdm/
     mkdir /usr/share/backgrounds
-    sudo cp ../../wallpapers/wallpapers/wallpaper.png /usr/share/backgrounds
+    sudo cp $dotfilesdir/wallpapers/wallpapers/wallpaper.png /usr/share/backgrounds
+    echo "Done."
 }
 
 ohmyzshinstaller() {
-    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    echo "Installing Oh-My-Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null
+    echo "Done."
 }
 
 dotfilesinstaller() {
+    echo "Setting up dotfiles..."
     cd $dotfilesdir
     stow --adopt *
     git restore .
     stow *
     cd $installdir 
+    echo "Done."
 } 
 
 paruinstaller() {
-    git clone https://aur.archlinux.org/paru.git
+    echo "Installing paru..."
+    git clone https://aur.archlinux.org/paru.git > /dev/null
     cd paru
-    makepkg -si 
+    makepkg -si > /dev/null
     cd $installdir
+    echo "Done."
 }
 
 lockscreenconf() {
@@ -66,18 +78,27 @@ lockscreenconf() {
 }
 
 vimpluginstaller() {
+    echo "Installing vim plugins..."
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    nvim -c PlugInstall -c qa!
+    echo "Done."
 }
 
 rangerplug() {
-    git clone https://github.com/alexanderjeurissen/ranger_devicons /home/$name/.config/ranger/plugins/ranger_devicons
+    git clone https://github.com/alexanderjeurissen/ranger_devicons /home/$name/.config/ranger/plugins/ranger_devicons > /dev/null
 }
 
 servicestarter() {
+    echo "Starting services..."
     sudo systemctl enable --now bluetooth.service 
+    echo "Done."
 }
 
+welcomemsg
+getuser
+
+# Packages to install
 fonts="ttf-firacode-nerd noto-fonts-emoji wqy-zenhei"
 apps="firefox audacity gimp ranger neovim vlc"
 tools="unzip reflector udisks2"
@@ -85,20 +106,26 @@ omega_dep="dunst git picom nitrogen wget stow rofi rofi-emoji pamixer bpytop zsh
 pipinstall="iwlib"
 aurinstall="fastfetch tomatoshell betterlockscreen"
 
-welcomemsg
-getuser
-getdotfilesdir
+# Directories
+installdir=echo pwd
+dotfilesdir="/home/$name/dotfiles"
+
+
 pacmanconf
 laptopconf
 paruinstaller
 rangerplug
 rm -rf /usr/share/backgrounds
-paru -Sy $fonts $apps $tools $omega_dep 
+echo "Installing packages:"
+paru -Sy $fonts $apps $tools $omega_dep
 paru -Sy $aurinstall
 pip install $pipinstall
+echo "Done."
 vimpluginstaller
 ohmyzshinstaller
 dotfilesinstaller
 lockscreenconf
 servicestarter
 lightdmconf
+
+endmsg
